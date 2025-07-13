323d03d2b2e841cdf07b6da2d7c55140f6649cec
Alphatech29 <alphatechmultimedia@gmail.com>
2025-07-13 13:57:22 +0100

    Refactor database configuration to use hardcoded values and enable connection logging

--- model/db.js
+++ model/db.js
@@ -2,23 +2,23 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const dbConfig = {
-  host: process.env.DEV_DB_HOST || "localhost",
-  user: process.env.DEV_DB_USER || "root",
-  password: process.env.DEV_DB_PASSWORD || "",
-  database: process.env.DEV_DB_NAME || "speednet",
-  port: parseInt(process.env.DB_PORT) || 9030,
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"speednet",
+  port: 9030,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   connectTimeout: 10000,
 };
 
-//console.log("Connecting to MySQL with config:", {
-  //host: dbConfig.host,
-  //user: dbConfig.user,
-  //database: dbConfig.database,
-  //port: dbConfig.port,
-//});
+console.log("Connecting to MySQL with config:", {
+  host: dbConfig.host,
+  user: dbConfig.user,
+  database: dbConfig.database,
+  port: dbConfig.port,
+});
 
 const pool = mysql.createPool(dbConfig).promise();
 
